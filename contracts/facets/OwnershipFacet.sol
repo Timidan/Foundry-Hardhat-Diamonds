21b28d3c40ee6dfbf66c0fddd1129c727779c712
karmabadger <karmabadger@outlook.com>
2022-07-16 19:06:49 -0400

    custom errors for ownership

--- contracts/facets/OwnershipFacet.sol
+++ contracts/facets/OwnershipFacet.sol
@@ -1,20 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import { IERC173 } from "../interfaces/IERC173.sol";
-import { LibOwnership } from "../libraries/LibOwnership.sol";
+import {IERC173} from "../interfaces/IERC173.sol";
+import {LibOwnership} from "../libraries/LibOwnership.sol";
+import {IDiamondInit} from "../interfaces/IDiamondInit.sol";
+
+error NotOwner();
 
 abstract contract OwnershipModifers {
   modifier onlyOwner() {
-    require(
-      msg.sender == LibOwnership.contractOwner(),
-      "Only the contract owner can call this function"
-    );
+    if (msg.sender != LibOwnership.diamondStorage().contractOwner)
+      revert NotOwner();
     _;
   }
 }
 
-contract OwnershipFacet is IERC173, OwnershipModifers {
+contract OwnershipFacet is IERC173 {
   function transferOwnership(address _newOwner) external override {
     LibOwnership.enforceIsContractOwner();
     LibOwnership.setContractOwner(_newOwner);
@@ -22,5 +23,11 @@ contract OwnershipFacet is IERC173, OwnershipModifers {
 
   function owner() external view override returns (address owner_) {
     owner_ = LibOwnership.contractOwner();
+  }
+}
+
+contract OwnershipDiamondInit is IDiamondInit {
+  function init() external {
+    LibOwnership.setContractOwner(msg.sender);
   }
 }
